c87dda029fffec2985318ee237ef5e996844cf83
Aleksey <48358627+AlekseyFedorov27@users.noreply.github.com>
2021-02-12 12:34:13 +0400

    Add files via upload

---
+++ dist/precache-manifest.fb91680277900e7c84c6c4a08aac0bfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499c4e30acec1f6a3be4",
+    "url": "css/app.aeab0b45.css"
+  },
+  {
+    "revision": "a8e2625aa46ef564a565",
+    "url": "css/chunk-vendors.1ea5a689.css"
+  },
+  {
+    "revision": "2bf321eb5b47923a0e621f0dabd62ec8",
+    "url": "index.html"
+  },
+  {
+    "revision": "499c4e30acec1f6a3be4",
+    "url": "js/app.005efcb6.js"
+  },
+  {
+    "revision": "a8e2625aa46ef564a565",
+    "url": "js/chunk-vendors.0a30f17b.js"
+  },
+  {
+    "revision": "28ff02ca053c32c28fab69a3fc1eb035",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
